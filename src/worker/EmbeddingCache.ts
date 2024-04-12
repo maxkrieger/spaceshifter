6e3e48012f8d7992435c509532578b31bd47336d
Max Krieger <maxkrieger@users.noreply.github.com>
2024-04-12 01:03:29 -0700

    chore: comments in embedding cache

--- src/worker/EmbeddingCache.ts
+++ src/worker/EmbeddingCache.ts
@@ -13,6 +13,7 @@ export default class EmbeddingCache {
   addToCache(add: EmbeddingCacheData) {
     this.cache = { ...this.cache, ...add };
   }
+
   getCache(): EmbeddingCacheData {
     return this.cache;
   }
@@ -23,6 +24,64 @@ export default class EmbeddingCache {
     }
     return this.cache[text];
   }
+
+  async fetchEmbeddingsFromOpenAI(
+    texts: string[],
+    apiKey: string,
+    model: string
+  ): Promise<void> {
+    log.info(`Fetching ${texts.length} embeddings`);
+
+    // Keeping the requests chunked by 150 seems to help throttling
+    const chunked = chunk(texts, 150);
+    for (let i = 0; i < chunked.length; i++) {
+      const chunk = chunked[i];
+      log.info(`Fetching ${chunk.length} embeddings chunk`);
+      const res = await backOff(() =>
+        fetch("https://api.openai.com/v1/embeddings", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${apiKey}`,
+          },
+          body: JSON.stringify({
+            model,
+            input: chunk,
+          }),
+        })
+      );
+
+      const json = await res.json();
+      if (res.status !== 200) {
+        log.error(json);
+        throw new Error(`Failed to fetch embeddings: ${json.error.message}`);
+      }
+
+      // Add to DB
+      const toAdd: Embedding[] = json.data.map(
+        (res: { index: number; embedding: number[] }) => ({
+          embedding: res.embedding,
+          text: chunk[res.index],
+          dateCreated: new Date(),
+          model,
+        })
+      );
+      await db.embeddings.bulkPut(toAdd);
+
+      // Add to cache
+      toAdd.forEach((res: Embedding) => {
+        this.cache[res.text] = res.embedding;
+      });
+
+      // Keep the progress bar updated
+      sendMessageToHost({
+        type: "embeddingProgress",
+        progress: i / chunked.length,
+      });
+    }
+    log.info(`Done fetching`);
+  }
+
   async bulkEmbed(
     pairs: Pairings,
     apiKey: string,
@@ -33,14 +92,17 @@ export default class EmbeddingCache {
       progress: 0,
     });
     log.info("Bulk embedding...");
+    // Collect all pieces of text
     const flattenedPairs = [
       ...new Set(pairs.flatMap((p) => [p.text_1, p.text_2])),
     ];
-    // For compound lookup
+    // Look up existing text embeddings by model in the DB
     const results = await db.embeddings.bulkGet(
       flattenedPairs.map((t) => [t, model])
     );
-    const missing = [];
+
+    // Missing text will be null in the results array. Add it to the missing queue
+    const missing: string[] = [];
     for (let i = 0; i < results.length; i++) {
       const res = results[i];
       if (res) {
@@ -51,49 +113,7 @@ export default class EmbeddingCache {
     }
 
     if (missing.length > 0) {
-      log.info(`Fetching ${missing.length} embeddings`);
-      const chunked = chunk(missing, 150);
-      for (let i = 0; i < chunked.length; i++) {
-        const chunk = chunked[i];
-        log.info(`Fetching ${chunk.length} embeddings chunk`);
-        const res = await backOff(() =>
-          fetch("https://api.openai.com/v1/embeddings", {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-              Authorization: `Bearer ${apiKey}`,
-            },
-            body: JSON.stringify({
-              model,
-              input: chunk,
-            }),
-          })
-        );
-
-        const json = await res.json();
-        if (res.status !== 200) {
-          log.error(json);
-          throw new Error(`Failed to fetch embeddings: ${json.error.message}`);
-        }
-        const toAdd: Embedding[] = json.data.map(
-          (res: { index: number; embedding: number[] }) => ({
-            embedding: res.embedding,
-            text: chunk[res.index],
-            dateCreated: new Date(),
-            model,
-          })
-        );
-        await db.embeddings.bulkPut(toAdd);
-        toAdd.forEach((res: Embedding) => {
-          this.cache[res.text] = res.embedding;
-        });
-
-        sendMessageToHost({
-          type: "embeddingProgress",
-          progress: i / chunked.length,
-        });
-      }
-      log.info(`Done fetching`);
+      await this.fetchEmbeddingsFromOpenAI(missing, apiKey, model);
     } else {
       log.info("No embeddings to fetch");
     }
