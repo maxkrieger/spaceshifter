92fde5299cf0058f2e74ed554fa0b072dfeb4781
Max Krieger <maxkrieger@users.noreply.github.com>
2024-04-12 18:57:07 -0700

    chore: comments on type defs

--- src/types.ts
+++ src/types.ts
@@ -1,16 +1,36 @@
 import { Tensor1D, Tensor2D, data, TensorContainer } from "@tensorflow/tfjs";
 import { defaultEmbeddingModel } from "./lib/const";
 
+/**
+ * Maps a piece of text to its embedding
+ */
 export type EmbeddingCacheData = { [key: string]: number[] };
 
+/**
+ * A point of training data
+ * @property text_1 - The first piece of text
+ * @property text_2 - The second piece of text
+ * @property label - The label. -1 for negative ("far apart"), 1 for positive ("close together")
+ */
 export type Pairing = {
   text_1: string;
   text_2: string;
   label: -1 | 1;
 };
 
+/**
+ * A list of training data
+ */
 export type Pairings = Pairing[];
 
+/**
+ * A tfjs dataset
+ * @property e1 - A tensor of embeddings for the first piece of text
+ * @property e2 - A tensor of embeddings for the second piece of text
+ * @property labels - A tensor of labels
+ * @property embeddingSize - The size (in parameters) of the embeddings
+ * @property tfDataset - The "zipped-up" tfjs dataset composed of the tensors
+ */
 export type TensorDataset = {
   e1: Tensor2D;
   e2: Tensor2D;
@@ -19,16 +39,28 @@ export type TensorDataset = {
   tfDataset: data.Dataset<TensorContainer>;
 };
 
+/**
+ * A subset of @TensorDataset
+ * Used for batching
+ */
 export type DatasetSlice = {
   e1: Tensor2D;
   e2: Tensor2D;
   labels: Tensor1D;
 };
 
-// [cosine similarity, label]
+/**
+ * A list of pairing similarities and groundtruth labels
+ * @property 0 - The observed cosine similarity between the two embeddings
+ * @property 1 - The groundtruth label. -1 for negative ("far apart"), 1 for positive ("close together")
+ */
 export type CosinePairings = [number, number][];
 
 export type OptimizerType = "gradient" | "adamax";
+
+/**
+ * Parameters for training the model
+ */
 export interface OptimizationParameters {
   dropoutFraction: number;
   learningRate: number;
@@ -53,12 +85,19 @@ export const defaultOptimizationParameters: OptimizationParameters = {
   embeddingModel: defaultEmbeddingModel,
 };
 
+/**
+ * Accuracy and standard error, as well as a human-readable rendering.
+ */
 export type AccuracyAndSE = {
   accuracy: number;
   se: number;
   message: string;
 };
 
+/**
+ * All performance metrics for a given matrix.
+ * Including the distribution of cosine similarities across datapoints.
+ */
 export type PerformanceGroup = {
   testCosinePairings: CosinePairings;
   trainCosinePairings: CosinePairings;
@@ -66,17 +105,31 @@ export type PerformanceGroup = {
   trainAccuracyAndSE: AccuracyAndSE;
 };
 
+/**
+ * Common elements for initializing the trainer
+ */
 type DatasetInitializer = {
   parameters: OptimizationParameters;
   pairings: Pairings;
 };
+
+/**
+ * Additional properties for initializing a local dataset
+ */
 export type LocalDatasetInitializer = {
   apiKey: string;
 } & DatasetInitializer;
+
+/**
+ * Additional properties for initializing an example dataset
+ */
 export type ExampleDatasetInitializer = {
   cacheUrl: string;
 } & DatasetInitializer;
 
+/**
+ * Message types one can send to the trainer worker
+ */
 export type MessageToTrainer =
   | ({
       type: "initializeLocalDataset";
@@ -90,6 +143,9 @@ export type MessageToTrainer =
     }
   | { type: "getEmbeddingCache" };
 
+/**
+ * Messages one can receive from the trainer worker
+ */
 export type MessageFromTrainer =
   | { type: "fetchingStatus"; status: "complete" | "fetching" }
   | {
@@ -113,6 +169,9 @@ export type MessageFromTrainer =
   | { type: "error"; message: string }
   | { type: "dumpEmbeddingCache"; cache: EmbeddingCacheData };
 
+/**
+ * A pointer to a local or example dataset
+ */
 export type DatasetLocator =
   | { type: "local"; id: number }
   | {
@@ -122,6 +181,10 @@ export type DatasetLocator =
       name: string;
     };
 
+/**
+ * A pointer to the current dataset.
+ * Examples include the fetched dataset itself for convenient access.
+ */
 export type CurrentDataset =
   | {
       type: "local";
@@ -135,6 +198,10 @@ export type CurrentDataset =
     }
   | { type: "none" };
 
+/**
+ * For visualizing the training process.
+ * Indexed by epoch.
+ */
 export type PerformanceHistory = {
   test: AccuracyAndSE;
   train: AccuracyAndSE;
