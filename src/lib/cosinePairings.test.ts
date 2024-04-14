c5de8bb0245f91ae67b1102c222d1104e7cb8c91
Max Krieger <maxkrieger@users.noreply.github.com>
2024-04-13 22:43:41 -0700

    chore: add vitest to ci

--- src/lib/cosinePairings.test.ts
+++ src/lib/cosinePairings.test.ts
@@ -7,20 +7,26 @@ import EmbeddingCache from "@/worker/EmbeddingCache";
 
 const cityFactsDataset = exampleDatasets.cityFacts;
 
-test("computeCosinePairings should produce groundtruth cosine values within 5 decimal places", async () => {
-  const cityFactsPairingsResult = await fetch(cityFactsDataset.datasetURL);
-  const cityFactsPairings = await cityFactsPairingsResult.json();
-  const cityFactsEmbeddingsResult = await fetch(cityFactsDataset.embeddingsURL);
-  const cityFactsEmbeddings = await cityFactsEmbeddingsResult.json();
-  const embeddingCache = new EmbeddingCache();
-  embeddingCache.addToCache(cityFactsEmbeddings);
+test(
+  "computeCosinePairings should produce groundtruth cosine values within 5 decimal places",
+  async () => {
+    const cityFactsPairingsResult = await fetch(cityFactsDataset.datasetURL);
+    const cityFactsPairings = await cityFactsPairingsResult.json();
+    const cityFactsEmbeddingsResult = await fetch(
+      cityFactsDataset.embeddingsURL
+    );
+    const cityFactsEmbeddings = await cityFactsEmbeddingsResult.json();
+    const embeddingCache = new EmbeddingCache();
+    embeddingCache.addToCache(cityFactsEmbeddings);
 
-  const tensorDataset = pairingToDataset(cityFactsPairings, embeddingCache);
-  const cosinePairings = await computeCosinePairings(tensorDataset);
-  for (let i = 0; i < cosinePairings.length; i++) {
-    expect(
-      cosinePairings[i][0],
-      `cosinePairings[${i}] is within 5 decimal places of groundtruth`
-    ).toBeCloseTo(citiesCosinePairingsGroundtruth[i][0], 5);
-  }
-});
+    const tensorDataset = pairingToDataset(cityFactsPairings, embeddingCache);
+    const cosinePairings = await computeCosinePairings(tensorDataset);
+    for (let i = 0; i < cosinePairings.length; i++) {
+      expect(
+        cosinePairings[i][0],
+        `cosinePairings[${i}] is within 5 decimal places of groundtruth`
+      ).toBeCloseTo(citiesCosinePairingsGroundtruth[i][0], 5);
+    }
+  },
+  { timeout: 15000 }
+);
